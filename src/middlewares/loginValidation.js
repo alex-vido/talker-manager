64c9ee4897e71e089baa4448778ea0b4f219a6c3
Alex Vido <alex.vido93@gmail.com>
2023-10-05 20:19:07 -0300

    req 4: validation login

---
+++ src/middlewares/loginValidation.js
@@ -0,0 +1,26 @@
+const crypto = require('crypto');
+
+function generateToken() {
+  return crypto.randomBytes(8).toString('hex');
+}
+
+const validEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+
+const loginValidation = (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' }); 
+  } if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  } if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  } if (!validEmail.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  } 
+  const token = generateToken();
+  res.status(200).json({ token });
+
+  next();
+};
+
+module.exports = loginValidation;
--- src/routes/talker.route.js
+++ src/routes/talker.route.js
@@ -1,12 +1,8 @@
 const talkRouter = require('express').Router();
 const fs = require('fs/promises');
-const crypto = require('crypto');
+const { loginValidation } = require('../middlewares');
 
 const FILE_PATH = 'src/talker.json';
-
-function generateToken() {
-  return crypto.randomBytes(8).toString('hex');
-}
 
 talkRouter.get('/talker', async (req, res) => {
   try {
@@ -37,30 +33,12 @@ talkRouter.get('/talker/:id', async (req, res) => {
   }
 });
 
-talkRouter.post('/login', async (req, res) => {
+talkRouter.post('/login', loginValidation, async (req, res) => {
   try {
-    const { email, password } = req.body;
-    if (!email || !password) {
-      return res.status(404).json({ message: 'Email e senha são obrigatórios' });
-    }
-    const token = generateToken();
-    res.status(200).json({ token });
+    console.log('');
   } catch (error) {
-    res.status(error.statusCode || 404).json({ message: error.message });
+    res.status(error.statusCode || 400).json({ message: error.message });
   }  
 });
-
-/*
-talkRouter.post('/talker/:talkerId', async (req, res) => {
-  const { name, age, talkWatchedAt, talkRate} = req.body;
-  const { talkerId } = req.params;
-
-  await connection
-    .execute('INSERT INTO talkers (id, name, age, talk_watched_at, talk_rate) VALUES (?, ?, ?, ?, ?)',
-      [Number(id), name, Number(age), talWatchedAt, talkRate]);
-
-  return res.status(201).json({ message: 'Insertion talker success' })
-});
-*/
 
 module.exports = talkRouter;
